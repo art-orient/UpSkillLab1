d624f22d0e1840516e5c4c33bfc071ca777f3f5d
Aliaksandr Artsikhovich <art-orient@tut.by>
2020-10-08 22:50:56 +0300

    LinearPrograms Task1

---
+++ src/LinearPrograms/Main.java
@@ -0,0 +1,10 @@
+package LinearPrograms;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 18;
+        int b = 23;
+        int c = 7;
+        System.out.println("Task 1:  z = " + Task1.task1(a, b, c));
+    }
+}
---
+++ src/LinearPrograms/Task1.java
@@ -0,0 +1,8 @@
+package LinearPrograms;
+// 1. Найдите  значение функции: z = ( (a – 3 ) * b / 2) + c.
+public class Task1 {
+    public static double task1 (int a, int b, int c) {
+        double z = (a - 3) * (double) b / 2 + c;
+        return z;
+    }
+}
