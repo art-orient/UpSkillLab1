590ebedef534ff5501d5a77fbeb64036e34a59af
Aliaksandr Artsikhovich <art-orient@tut.by>
2020-10-09 23:07:42 +0300

    Branching Task 5

--- src/Branching/Main.java
+++ src/Branching/Main.java
@@ -10,6 +10,6 @@ public class Main {
         System.out.println("Task 3:  Сумма квадратов первых 100 чисел = " + Task3.task3());
         System.out.println("Task 4:  Произведение квадратов первых двухсот чисел = ");
         System.out.println(Task4.task4());
-
+        Task5.task5();
     }
 }
---
+++ src/Branching/Task5.java
@@ -0,0 +1,12 @@
+package Branching;
+// 5. Вывести на экран соответствий между символами и их численными обозначениями в памяти компьютера.
+public class Task5 {
+    public static int task5 () {
+        System.out.println("Task 5:  Вывод соответствий между символами и их численными обозначениями в памяти компьютера");
+        int sum = 0;
+        for (int i = 33; i <= 255; i++) {
+            System.out.println(i + " - " + (char)i + "\t");
+        }
+        return sum;
+    }
+}
