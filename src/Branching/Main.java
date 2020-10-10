dd6dbb3df18dd7e49a2a32d476eda27f48d5ea91
Aliaksandr Artsikhovich <art-orient@tut.by>
2020-10-10 09:43:00 +0300

    Branching Task 3

--- src/Branching/Main.java
+++ src/Branching/Main.java
@@ -10,7 +10,14 @@ public class Main {
         int c = 3;
         int d = 532;
         System.out.println("Task 2: result = " + Task2.task2(a, b, c, d));
-        Task3.task3();
+        int x1, y1, x2, y2, x3, y3;
+        x1 = 3;
+        y1 = 7;
+        x2 = 5;
+        y2 = 10;
+        x3 = 7;
+        y3 = 13;
+        System.out.println("Task 3: " + Task3.task3(x1, y1, x2, y2, x3, y3));
         Task4.task4();
         Task5.task5();
     }
