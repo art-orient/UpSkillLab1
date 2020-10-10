06d506c447f311f3f352f67b3b8883ab4c802fb1
Aliaksandr Artsikhovich <art-orient@tut.by>
2020-10-10 09:15:42 +0300

    Branching Task 2

--- src/Branching/Main.java
+++ src/Branching/Main.java
@@ -5,7 +5,11 @@ public class Main {
         int a = 48;
         int b = 42;
         Task1.task1(a, b);
-        Task2.task2();
+        a = 324;
+        b = 15;
+        int c = 3;
+        int d = 532;
+        System.out.println("Task 2: result = " + Task2.task2(a, b, c, d));
         Task3.task3();
         Task4.task4();
         Task5.task5();
