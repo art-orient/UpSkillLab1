446b4a553103d22d7f07ea85ca643f24cf7c3f0f
Aliaksandr Artsikhovich <art-orient@tut.by>
2020-10-19 16:04:18 +0300

    task10 update

--- src/Algorithmization/ArraysOfArrays/Task10.java
+++ src/Algorithmization/ArraysOfArrays/Task10.java
@@ -4,10 +4,8 @@ public class Task10 {
     public static void findPositiveOfDiagonal (int[][] array) {
         System.out.print("Положительные элементы главной диагонали = ");
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                if (i == j && array[i][j] > 0) {
-                    System.out.print(array[i][j] + " ");
-                }
+            if (array[i][i] > 0) {
+                System.out.print(array[i][i] + " ");
             }
         }
         System.out.println();
