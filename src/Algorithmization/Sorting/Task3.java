26c882805f09afd4abc2f0b29681438b07fa79af
Aliaksandr Artsikhovich <art-orient@tut.by>
2020-10-20 21:35:42 +0300

    task3 update

--- src/Algorithmization/Sorting/Task3.java
+++ src/Algorithmization/Sorting/Task3.java
@@ -4,8 +4,8 @@ package Algorithmization.Sorting;
 public class Task3 {
     public static int[] sortBubble (int[] array) {
         long start = System.currentTimeMillis();
-        boolean isSort = true;
         for (int i = 0; i < array.length; i++) {
+            boolean isSort = true;
             for (int j = 1; j < array.length - i; j++) {
                 if (array[j] < array[j-1]) {
                     int temp = array[j];
