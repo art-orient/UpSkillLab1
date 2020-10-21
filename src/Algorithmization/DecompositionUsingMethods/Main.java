6ea1b574d66589b7693fc338183b57e3077bdacd
Aliaksandr Artsikhovich <art-orient@tut.by>
2020-10-21 15:44:00 +0300

    task 1

--- src/Algorithmization/DecompositionUsingMethods/Main.java
+++ src/Algorithmization/DecompositionUsingMethods/Main.java
@@ -1,7 +1,12 @@
 package Algorithmization.DecompositionUsingMethods;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Task1.findNOD();
+
+
 
     }
 }
--- src/Algorithmization/DecompositionUsingMethods/Task1.java
+++ src/Algorithmization/DecompositionUsingMethods/Task1.java
@@ -1,6 +1,28 @@
 package Algorithmization.DecompositionUsingMethods;
+
+import java.util.Scanner;
+
 // 1. Написать метод(методы) для нахождения наибольшего общего делителя и наименьшего общего
 // кратного двух натуральных чисел: ...
 public class Task1 {
-
+    public static void findNOD () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое натуральное число: ");
+        int inputA = scanner.nextInt();
+        System.out.print("Введите второе натуральное число: ");
+        int inputB = scanner.nextInt();
+        if (inputA < 1 || inputB < 1) {
+            System.out.println("Введенные числа не являются натуральными. Попробуйте еще раз.");
+            return;
+        }
+        int nod = inputA;
+        int b = inputB;
+        while (b != 0){
+            int temp = nod % b;
+            nod = b;
+            b = temp;
+        }
+        System.out.println("Наибольший общий делитель = " + nod);
+        System.out.println("Наибольшee общee кратное = " + (inputA * inputB)/nod);
+    }
 }
